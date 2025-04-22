4e794cf1d614c3bf9cb0ca6cd7e47892de8f6fee
Catherine Zheng <129779755+dfjhi@users.noreply.github.com>
2025-04-21 23:24:52 -0700

    updates

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -1,21 +1,10 @@
-// src/main.jsx
-import React from "react";
-import ReactDOMClient from "react-dom/client";
-import "./main.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-function MyApp() {
-  return (
-    <div>
-      <h1>Hello, React!</h1>
-    </div>
-  );
-}
-
-// Create the container
-const container = document.getElementById("root");
-
-// Create a root
-const root = ReactDOMClient.createRoot(container);
-
-// Initial render: Render an element to the Root
-root.render(<MyApp />);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
